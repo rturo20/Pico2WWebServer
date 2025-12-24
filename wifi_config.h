88b6ab71d205050ed38f9e2e93f1710b10454e7d
rturo20 <rturo20@icloud.com>
2025-12-24 16:25:17 +0000

    Original Files

---
+++ wifi_config.h
@@ -0,0 +1,11 @@
+// WiFi Configuration
+// This file is in .gitignore and will NOT be committed to git
+
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+const char WIFI_SSID[] = "Treviso";
+const char WIFI_PASSWORD[] = "Garbin@1921";
+
+#endif // WIFI_CONFIG_H
+
